9bfcd5ac8045a3f45020a89829c11d36d89722e2
june65 <june0100726@gmail.com>
2023-04-19 02:54:07 +0900

    Add fire_base

--- dogmbti/lib/main.dart
+++ dogmbti/lib/main.dart
@@ -1,10 +1,16 @@
 import 'dart:typed_data';
+import 'package:dogmbti/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img;
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:tflite_flutter/tflite_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
